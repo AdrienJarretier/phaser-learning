79bf27b9f2765191622dc05df5a541059a782a1c
Jarretier Adrien <jarretier.adrien@gmail.com>
2016-04-30 18:19:53 +0200

    testing hellophaser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var app = require("express")();
+var express = require("express");
+var app = express();
 var http = require("http").Server(app);
 
 app.get('/', function(req, res){
@@ -6,6 +7,8 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 
 });
+
+app.use('/hellophaser', express.static('hellophaser'));
 
 http.listen(80, function(){
   console.log('listening on *:80');
