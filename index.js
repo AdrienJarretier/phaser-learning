4fbd6ccbdc6dc3f1a9bc33b04003fcd16335de61
Jarretier Adrien <jarretier.adrien@gmail.com>
2016-04-30 18:49:08 +0200

    tutorial 02 files, first game

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get('/', function(req, res){
 });
 
 app.use('/hellophaser', express.static('hellophaser'));
+app.use('/phaser_tutorial_02_01', express.static('phaser_tutorial_02/part1.html'));
 
 http.listen(80, function(){
   console.log('listening on *:80');
