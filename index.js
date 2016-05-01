4c9ebbdf8e9756598221d4b0c0d9a0040491adbe
Jarretier Adrien <jarretier.adrien@gmail.com>
2016-05-01 14:42:50 +0200

    preparation for new exampl about platforms

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ app.get('/', function(req, res){
 
 });
 
-app.use('/hellophaser', express.static('hellophaser'));
-app.use('/phaser_tutorial_02', express.static('phaser_tutorial_02'));
+app.use( express.static('games') );
+
+// app.use('/hellophaser', express.static('hellophaser'));
+// app.use('/phaser_tutorial_02', express.static('phaser_tutorial_02'));
+// app.use('/Ride-on_Platforms', express.static('Ride-on_Platforms'));
+// app.use('/phaser.min.js', express.static('phaser.min.js'));
 
 http.listen(80, function(){
   console.log('listening on *:80');
